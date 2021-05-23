47b3acf48fe937558c2a588da76dfbdd6843c42a
shah1012 <69828183+shah1012@users.noreply.github.com>
2021-05-22 19:24:21 -0500

    Forgot to add app.use

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 const spotify = require("./routes/spotify");
+const search = require("./routes/search");
 
 app.use(express.json());
 
 app.use("/spotify", spotify);
+
+app.use("/spotify/search", search);
 
 app.listen(3001, () => {
   console.log("Listening on port 3001");
