9d37fb24cb04bb8e899718e0effb6ae74c9ea963
Satvik Seeram <satvikgsu@gmail.com>
2021-05-23 06:54:52 +0530

    login path

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
 const app = express();
+<<<<<<< HEAD
+const { route: spotify } = require("./routes/spotify");
+=======
 const spotify = require("./routes/spotify");
+const search = require("./routes/search");
+>>>>>>> 47b3acf48fe937558c2a588da76dfbdd6843c42a
 
 app.use(express.json());
 
 app.use("/spotify", spotify);
+
+app.use("/spotify/search", search);
 
 app.listen(3001, () => {
   console.log("Listening on port 3001");
--- routes/spotify.js
+++ routes/spotify.js
@@ -1,14 +1,50 @@
+//sportifyApi
+
 const SpotifyWebApi = require("spotify-web-api-node");
+
+//exoress
 const express = require("express");
-require("dotenv").config();
+const route = express.Router();
+
+//dotEnv
+require("dotenv").config(); //dotenv isnt working we need to look at it later
 
 //credentials
-
+// we need to put this in a dotenv file after setting it up
 const credentials = {
-  clientId: process.env.client_id,
-  clientSecret: process.env.client_Secret,
-  redirectUri: redirect_uri,
+  clientId: "0e098488165c416b8f3451c972feee06",
+  clientSecret: "689ce50f2eef4ca4bc6450d19e3e1a1d",
+  redirectUri: "http://localhost:3000",
 };
+var spotifyApi = new SpotifyWebApi(credentials); // use this since im setting the access token in this instance
+
+route.post("/login", async (req, res) => {
+  // post request since we need the code from the client
+  const code = req.body.code;
+
+  try {
+    // authenticating the code and getting the data
+    const result = await spotifyApi.authorizationCodeGrant(code);
+    const tokenData = {
+      tokenExpire: result.body["expires_in"],
+      tokenAccess: result.body["access_token"],
+      refreshToken: result.body["refresh_token"],
+    };
+
+    spotifyApi.setAccessToken(result.body["access_token"]);
+    spotifyApi.setRefreshToken(result.body["refresh_token"]);
+    res.send(tokenData);
+  } catch (err) {
+    res.send(err).status(401);
+  }
+});
+
+module.exports = {
+  route,
+  spotifyApi,
+};
+<<<<<<< HEAD
+=======
 
 var spotifyApi = new SpotifyWebApi(credentials);
 
@@ -38,3 +74,6 @@ var spotifyApi = new SpotifyWebApi(credentials);
 //       console.log('Something went wrong!', err);
 //     }
 //   );
+
+module.exports = spotifyApi;
+>>>>>>> 47b3acf48fe937558c2a588da76dfbdd6843c42a
