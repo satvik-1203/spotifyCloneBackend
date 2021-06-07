e5d616a68bcb1830feab85271fff4a64cc8fffd0
Satvik Seeram <satvikgsu@gmail.com>
2021-06-08 00:50:59 +0530

    login class

---
+++ src/classes/SpotifyClass.js
@@ -0,0 +1,41 @@
+const SpotifyWebApi = require("spotify-web-api-node");
+const Token = require("../schema/refreshToken");
+
+const credentials = {
+  clientId: "0e098488165c416b8f3451c972feee06",
+  clientSecret: "689ce50f2eef4ca4bc6450d19e3e1a1d",
+  redirectUri: "http://localhost:3000",
+};
+
+class SpotifyClass extends SpotifyWebApi {
+  constructor() {
+    super(credentials);
+  }
+
+  async login(req, res) {
+    try {
+      const code = req.body.code;
+
+      const result = await super.authorizationCodeGrant(code);
+
+      const tokenData = {
+        tokenExpire: result.body["expires_in"],
+        tokenAccess: result.body["access_token"],
+        refreshToken: result.body["refresh_token"],
+      };
+      const token = new Token({
+        accessToken: result.body["access_token"],
+        refreshToken: result.body["refresh_token"],
+      });
+
+      await token.save();
+      console.log(result.body);
+      res.send(tokenData);
+    } catch (err) {
+      res.status(401).send("Error from the backend");
+      console.log(err);
+    }
+  }
+}
+
+module.exports = SpotifyClass;
--- src/routes/spotify/login.js
+++ src/routes/spotify/login.js
@@ -1,6 +1,6 @@
 const SpotifyWebApi = require("spotify-web-api-node");
 
-//exoress
+//express
 const express = require("express");
 const route = express.Router();
 
@@ -9,7 +9,9 @@ const Token = require("../../schema/refreshToken");
 // other spotify Routes
 
 //dotEnv
-require("dotenv").config(); //dotenv isnt working we need to look at it later
+require("dotenv").config; //dotenv isnt working we need to look at it later
+
+const SpotifyClass = require("../../classes/SpotifyClass");
 
 //credentials
 // we need to put this in a dotenv file after setting it up
@@ -19,42 +21,45 @@ const credentials = {
   redirectUri: "http://localhost:3000",
 };
 
-let spotifyApi = new SpotifyWebApi(credentials); // use this since im setting the access token in this instance
+// let spotifyApi = new SpotifyWebApi(credentials); // use this since im setting the access token in this instance
+const spotifyClass = new SpotifyClass();
 
 route.post("/", async (req, res) => {
   // post request since we need the code from the client
-  const code = req.body.code;
-  try {
-    // authenticating the code and getting the data
-    const result = await spotifyApi.authorizationCodeGrant(code);
-    const tokenData = {
-      tokenExpire: result.body["expires_in"],
-      tokenAccess: result.body["access_token"],
-      refreshToken: result.body["refresh_token"],
-    };
-    const token = new Token({
-      accessToken: result.body["access_token"],
-      refreshToken: result.body["refresh_token"],
-    });
-
-    try {
-      spotifyApi.setAccessToken(result.body["access_token"]);
-      spotifyApi.setRefreshToken(result.body["refresh_token"]);
-    } catch (err) {
-      console.log("Couldn't set the token");
-    }
-
-    try {
-      res.send(tokenData);
-      await token.save();
-    } catch (err) {
-      console.log("Already exists");
-    }
-  } catch (err) {
-    res.send("error from backend").status(401);
-  }
+  spotifyClass.login(req, res);
 });
 module.exports = {
   route,
-  spotifyApi,
+  // spotifyApi,
 };
+
+// const code = req.body.code;
+//   try {
+//     // authenticating the code and getting the data
+//     const result = await spotifyApi.authorizationCodeGrant(code);
+//     const tokenData = {
+//       tokenExpire: result.body["expires_in"],
+//       tokenAccess: result.body["access_token"],
+//       refreshToken: result.body["refresh_token"],
+//     };
+//     const token = new Token({
+//       accessToken: result.body["access_token"],
+//       refreshToken: result.body["refresh_token"],
+//     });
+
+//     try {
+//       spotifyApi.setAccessToken(result.body["access_token"]);
+//       spotifyApi.setRefreshToken(result.body["refresh_token"]);
+//     } catch (err) {
+//       console.log("Couldn't set the token");
+//     }
+
+//     try {
+//       res.send(tokenData);
+//       await token.save();
+//     } catch (err) {
+//       console.log("Already exists");
+//     }
+//   } catch (err) {
+//     res.send("error from backend").status(401);
+//   }
